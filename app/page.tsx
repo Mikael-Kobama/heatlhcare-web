0db90a4f1cdfbb7215c20fdd1c9f4d33a1985704
Sr. Kobama <125419153+Mikael-Kobama@users.noreply.github.com>
2025-04-14 23:31:38 -0300

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+const Home = ({ searchParams }: SerchParamsProps) => {
+  const isAdmin = searchParams?.admin === "true";
+
   return (
-    <div>
+    <div className="flex h-screen max-h-screen">
       <h1 className="text-3xl underline">Home!</h1>
     </div>
   );
-}
+};
+
+export default Home;
