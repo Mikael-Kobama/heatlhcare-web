8674a1f7346f00b4ce404275a097c1f91782257c
Sr. Kobama <125419153+Mikael-Kobama@users.noreply.github.com>
2025-04-15 22:09:44 -0300

    update

---
+++ components/forms/PatientForm.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
---
+++ lib/actions/patient.actions.ts
@@ -0,0 +1,28 @@
+"use server";
+
+import { ID, InputFile, Query } from "node-appwrite";
+
+import {
+  BUCKET_ID,
+  DATABASE_ID,
+  ENDPOINT,
+  PATIENT_COLLECTION_ID,
+  PROJECT_ID,
+  databases,
+  storage,
+  users,
+} from "../appwrite.config";
+import { parseStringify } from "../utils";
+
+
+export const createUser = async ( user: CreateUserParams) => {
+  try {
+    const newUser = await users.create(
+      ID.unique(),
+      user.email,
+      user.phone,
+      undefined,
+      user.name
+    )
+  }
+}
